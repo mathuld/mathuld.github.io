a1353c7521a307fbf5c390905f8cde9e610d4418
Mathieu VINCENT <mathieu.vincent1997@orange.fr>
2017-10-24 15:56:48 +0200

    coucou c moa

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Mathieu VINCENT</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Mathieu VINCENT</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Accueil</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Projets <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <!-- <li><a href="#">Page 2</a></li> -->
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Me contacter</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-download"></span> Télécharger mon CV</a></li>
+    </ul>
+  </div>
+</nav>
+  
+      <div class="page-header text-center">
+        <h1>Mathieu VINCENT</h1>
+        <p>Réseaux Informatiques et Communication Multimédia</p>
+    </div>
+
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="images/images.jpeg" alt="">
+    </div>
+    <div class="col-sm-8">
+      <h3>Qui je suis ?</h3>
+      <p>Intéressé par le domaine de l'Informatique, j'ai décider d'effectuer DUT
+      dans le domaine des réseaux et des télécommunications. Après avoir effectué une
+      année en alternance, je décide d'intégrer Polytech Grenoble dans la formation RICM 
+      (Réseaux Informatiques et Communication Multimédia) afin de me spécialiser. 
+      
+      Curieux et minutieux, je suis en recherche constante de nouvelles connaissances à 
+      la pointe de la technologie.  
+      </p>
+
+      <hr>
+
+      <h3>Compétences</h3>
+      <div class="panel panel-default">
+      <div class="panel-heading">Développement</div>
+      <div class="panel-body">
+      
+      <p>Java</p>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+        </div>
+      </div>
+
+      <p>C</p>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:30%">
+        </div>
+      </div>
+
+      <p>HTML</p>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <h3>Experiences</h3>
+
+
+      
+
+    </div>
+  </div>
+</div>
+
+
+</body>
+
+<footer>
+</footer>
